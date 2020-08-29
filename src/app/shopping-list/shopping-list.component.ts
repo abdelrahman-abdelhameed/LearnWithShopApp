9923de13659b361b35ccddc7a90fd410b548b29f
Abdelrahman Abdelhameed <abdelrahmanabdelhameed@hotmail.com>
2020-08-30 00:46:50 +0200

    shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../Shared/ingredient.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredient : Ingredient[] = [
+   new Ingredient("eggs" , "5"),
+   new Ingredient("onine" , "1"),
+  ];
   constructor() { }
 
   ngOnInit(): void {
